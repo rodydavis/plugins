b5a65c02d7e14bbe123b3e34ea37bc93e6d03651
Rody Davis <rody.davis.jr@gmail.com>
2018-09-26 13:42:55 -0400

    adding scales List

--- packages/sheet_music/CHANGELOG.md
+++ packages/sheet_music/CHANGELOG.md
@@ -1,3 +1,7 @@
+## [0.0.2] - Added Functions
+
+* Added Utility Fuctions for Getting Assets Names
+
 ## [0.0.1] - TODO: Add release date.
 
 * Added Sheet Music View
---
+++ packages/sheet_music/lib/util/scales.dart
@@ -0,0 +1,17 @@
+const List<String> scales = <String>[
+  "C Major",
+  "G Major",
+  "D Major",
+  "A Major",
+  "E Major",
+  "B Major",
+  "F# Major",
+  "C# Major",
+  "F Major",
+  "Bb Major",
+  "Eb Major",
+  "Ab Major",
+  "Db Major",
+  "Gb Major",
+  "Cb Major",
+];
