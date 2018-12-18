7bd4360021910a83b17bab2eb25260c6c13c6762
Rody Davis <rody.davis.jr@gmail.com>
2018-12-17 20:39:34 -0500

    Updating switch

--- packages/native_widgets/lib/switch/switch.dart
+++ packages/native_widgets/lib/switch/switch.dart
@@ -5,34 +5,31 @@ class NativeSwitch extends StatelessWidget {
   final bool value;
   final ValueChanged<bool> onChanged;
   final Color activeColor;
-  final bool showMaterial;
 
   NativeSwitch({
     Key key,
     @required this.value,
     @required this.onChanged,
     this.activeColor,
-    this.showMaterial = false,
   });
 
   @override
   Widget build(BuildContext context) {
-    final bool _isIos = showCupertino(showMaterial: showMaterial);
-
-    if (_isIos) {
-      return CupertinoSwitch(
-        key: key,
-        value: value,
-        onChanged: onChanged,
-        activeColor:
-            activeColor == null ? CupertinoColors.activeGreen : activeColor,
-      );
-    }
-    return Switch(
+    return PlatformWidget(
       key: key,
-      value: value,
-      onChanged: onChanged,
-      activeColor: activeColor,
+      ios: (BuildContext context) => CupertinoSwitch(
+            key: key,
+            value: value,
+            onChanged: onChanged,
+            activeColor:
+                activeColor == null ? CupertinoColors.activeGreen : activeColor,
+          ),
+      android: (BuildContext context) => Switch(
+            key: key,
+            value: value,
+            onChanged: onChanged,
+            activeColor: activeColor,
+          ),
     );
   }
 }
