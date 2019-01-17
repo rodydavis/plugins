7f28a8c70f99de268d8cfb0d3dc0ebfd89b311b9
Rody Davis <rdavis@unifycrm.com>
2019-01-16 21:54:06 -0500

    Updating

---
+++ packages/cupertino_controllers/example/ios/Pods/Target Support Files/cupertino_controllers/cupertino_controllers-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_cupertino_controllers : NSObject
+@end
+@implementation PodsDummy_cupertino_controllers
+@end
---
+++ packages/cupertino_controllers/example/ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,14 @@
+//
+//  Generated file. Do not edit.
+//
+
+#import "GeneratedPluginRegistrant.h"
+#import <cupertino_controllers/CupertinoControllersPlugin.h>
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [CupertinoControllersPlugin registerWithRegistrar:[registry registrarForPlugin:@"CupertinoControllersPlugin"]];
+}
+
+@end
