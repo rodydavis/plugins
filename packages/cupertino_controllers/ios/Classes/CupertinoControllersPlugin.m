c45470c7bb644f7ae8ed60c0458690478deec789
Rody Davis <rdavis@unifycrm.com>
2019-02-22 15:23:00 -0500

    Upating

--- packages/cupertino_controllers/ios/Classes/CupertinoControllersPlugin.m
+++
@@ -1,8 +0,0 @@
-#import "CupertinoControllersPlugin.h"
-#import <cupertino_controllers/cupertino_controllers-Swift.h>
-
-@implementation CupertinoControllersPlugin
-+ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  [SwiftCupertinoControllersPlugin registerWithRegistrar:registrar];
-}
-@end
