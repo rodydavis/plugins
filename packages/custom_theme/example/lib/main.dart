8e7f7851f6b00031adca518e458d2d14070bf0d8
Rody Davis <rdavis@unifycrm.com>
2019-02-25 09:21:08 -0500

    custom_theme base

---
+++ packages/custom_theme/example/lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+
+import 'package:flutter/services.dart';
+import 'package:custom_theme/custom_theme.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Plugin example app'),
+        ),
+        body: Center(
+          child: Text('Running on...'),
+        ),
+      ),
+    );
+  }
+}
