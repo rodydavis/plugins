9ea6e08ff138fe7a9f02598990b271269f317254
Rody Davis <rdavis@unifycrm.com>
2019-02-25 09:04:48 -0500

    SMS Update

--- packages/flutter_sms/android/src/main/java/com/appleeducate/fluttersms/FlutterSmsPlugin.java
+++ packages/flutter_sms/android/src/main/java/com/appleeducate/fluttersms/FlutterSmsPlugin.java
@@ -1,21 +1,21 @@
 package com.appleeducate.fluttersms;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.provider.Settings;
+import android.util.Log;
+
+import java.util.ArrayList;
+
+import androidx.core.app.ActivityCompat;
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
 import io.flutter.plugin.common.PluginRegistry.Registrar;
-import android.app.Activity;
-import android.content.Intent;
-
-import android.Manifest;
-import android.content.pm.PackageManager;
-import android.provider.Settings;
-import androidx.core.app.ActivityCompat;
-import android.util.Log;
-import android.net.Uri;
-
-import java.util.ArrayList;
 
 /** FlutterSmsPlugin */
 public class FlutterSmsPlugin implements MethodCallHandler {
@@ -32,18 +32,8 @@ public class FlutterSmsPlugin implements MethodCallHandler {
     if (call.method.equals("sendSMS")) {
       String message = call.argument("message");
       ArrayList<String> recipients = call.argument("recipients");
-      String smsPermission = "SEND_SMS";
-      if (!checkPermission(smsPermission)) {
-        requestPermission(smsPermission);
-        if (!checkPermission(smsPermission)) {
-          openSettings();
-        } else {
-          sendSMS(recipients, message);
-        }
-      } else {
-        sendSMS(recipients, message);
-      }
-      result.success("Android " + android.os.Build.VERSION.RELEASE);
+      sendSMS(recipients, message);
+      result.success("SMS Sent!" );
     } else {
       result.notImplemented();
     }
@@ -54,43 +44,8 @@ public class FlutterSmsPlugin implements MethodCallHandler {
   }
 
   private void sendSMS(ArrayList<String> phones, String message) {
-    Intent sendIntent = new Intent(Intent.ACTION_VIEW);
+    Intent sendIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("sms:" + phones));
     sendIntent.putExtra("sms_body", message);
-    sendIntent.setData(Uri.parse("sms:" + phones));
     activity.startActivity(sendIntent);
-  }
-
-  private boolean checkPermission(String permission) {
-    permission = getManifestPermission(permission);
-    Log.i("SimplePermission", "Checking permission : " + permission);
-    return PackageManager.PERMISSION_GRANTED == ActivityCompat.checkSelfPermission(activity, permission);
-  }
-
-  private void requestPermission(String permission) {
-    permission = getManifestPermission(permission);
-    Log.i("SimplePermission", "Requesting permission : " + permission);
-    String[] perm = { permission };
-    ActivityCompat.requestPermissions(activity, perm, 0);
-  }
-
-  private String getManifestPermission(String permission) {
-    String res;
-    switch (permission) {
-    case "SEND_SMS":
-      res = Manifest.permission.SEND_SMS;
-      break;
-    default:
-      res = "ERROR";
-      break;
-    }
-    return res;
-  }
-
-  private void openSettings() {
-    Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
-        Uri.parse("package:" + activity.getPackageName()));
-    intent.addCategory(Intent.CATEGORY_DEFAULT);
-    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-    activity.startActivity(intent);
   }
 }
