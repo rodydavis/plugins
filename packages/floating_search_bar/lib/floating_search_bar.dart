d3f5e07993d9c40ebaa0a83704bfd602e19ddba4
Rody Davis <rdavis@unifycrm.com>
2019-03-13 11:42:46 -0400

    Updating App Bar

--- packages/floating_search_bar/lib/floating_search_bar.dart
+++ packages/floating_search_bar/lib/floating_search_bar.dart
@@ -1,31 +1,65 @@
-import 'dart:async';
-
-import 'package:floating_search_bar/ui/sliver_search_bar.dart';
-import 'package:flutter/rendering.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter/material.dart';
-import 'dart:math' as math;
 
-class FloatingSearchBar extends StatelessWidget {
-  const FloatingSearchBar({
+import 'ui/sliver_search_bar.dart';
+export 'ui/sliver_search_bar.dart';
+
+class FloatingSearchBar extends StatefulWidget {
+  FloatingSearchBar({
     this.body,
     this.drawer,
     this.trailing,
     this.leading,
-  });
+    this.controller,
+    List<Widget> children,
+  }) : _childDelagate = SliverChildListDelegate(
+          children,
+        );
+
+  FloatingSearchBar.builder({
+    this.body,
+    this.drawer,
+    this.trailing,
+    this.leading,
+    this.controller,
+    IndexedWidgetBuilder itemBuilder,
+    int itemCount,
+  }) : _childDelagate = SliverChildBuilderDelegate(
+          itemBuilder,
+          childCount: itemCount,
+        );
 
   final Widget leading, trailing, body, drawer;
+  final SliverChildDelegate _childDelagate;
+  final TextEditingController controller;
+
+  @override
+  _FloatingSearchBarState createState() => _FloatingSearchBarState();
+}
+
+class _FloatingSearchBarState extends State<FloatingSearchBar> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer: drawer,
+      drawer: widget.drawer,
       body: CustomScrollView(
         slivers: <Widget>[
-          SliverSearchBar(
-            leading: leading,
+          SliverFloatingBar(
+            leading: widget.leading,
             floating: true,
+            title: TextField(
+              controller: widget.controller,
+              decoration: InputDecoration.collapsed(
+                hintText: "Search...",
+              ),
+              autofocus: false,
+            ),
+            trailing: CircleAvatar(
+              child: Text("A"),
+            ),
           ),
-          SliverToBoxAdapter(child: body ?? Container()),
+          SliverList(
+            delegate: widget._childDelagate,
+          ),
         ],
       ),
     );
