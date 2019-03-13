8bd58b0fc2dfde2ce3d20f6a7de70ae7a5502877
Rody Davis <rdavis@unifycrm.com>
2019-03-13 15:15:54 -0400

    Update main.dart

--- packages/responsive_scaffold/example/lib/main.dart
+++ packages/responsive_scaffold/example/lib/main.dart
@@ -15,14 +15,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Plugin example app'),
-        ),
-        body: Center(
-          child: Text('Running on'),
-        ),
-      ),
+      home: ResponsiveScaffold(),
     );
   }
 }
