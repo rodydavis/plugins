633816e67cde4133a6b95510b2e3f44d8ebef7c1
Rody Davis <rdavis@unifycrm.com>
2019-03-13 15:15:22 -0400

    Update responsive_scaffold.dart

--- packages/responsive_scaffold/lib/responsive_scaffold.dart
+++ packages/responsive_scaffold/lib/responsive_scaffold.dart
@@ -1,7 +1,24 @@
 import 'dart:async';
-
+import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-class ResponsiveScaffold {
- 
+class ResponsiveScaffold extends StatelessWidget {
+  ResponsiveScaffold({
+    this.tabletBreakpoint = const Size(480.0, 480.0),
+  });
+
+  final Size tabletBreakpoint;
+
+  @override
+  Widget build(BuildContext context) {
+    final Size _size = MediaQuery.of(context).size;
+    if (_size.width >= tabletBreakpoint.width &&
+        _size.height >= tabletBreakpoint.height) {
+      // Tablet
+      return Container();
+    }
+
+    // Mobile
+    return Container();
+  }
 }
