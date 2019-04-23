f0e7d2b1081842760d15bf087123f2a16d02c3cb
Rody Davis <rody.davis.jr@gmail.com>
2019-04-23 09:54:17 -0400

    flutter_ar base

---
+++ packages/flutter_ar/example/lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+
+import 'package:flutter/services.dart';
+import 'package:flutter_ar/flutter_ar.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Plugin example app'),
+        ),
+        body: Center(
+          child: Text('Running on: \n'),
+        ),
+      ),
+    );
+  }
+}
---
+++ packages/flutter_ar/lib/flutter_ar.dart
@@ -0,0 +1,5 @@
+import 'dart:async';
+
+import 'package:flutter/services.dart';
+
+class FlutterAr {}
