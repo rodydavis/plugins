4f585627d019a80797888ce1d2ebab104ddca1e7
Jorgen Nystad <jn@avinet.no>
2019-04-24 13:22:21 +0200

    flutter_sms: Fix crash when recipients are empty

--- packages/flutter_sms/lib/flutter_sms.dart
+++ packages/flutter_sms/lib/flutter_sms.dart
@@ -18,11 +18,7 @@ class FlutterSms {
       final String result = await _channel.invokeMethod('sendSMS', mapData);
       return result;
     } else {
-      String _phones = "";
-      for (var p in recipients) {
-        _phones += p + ",";
-      }
-      _phones = _phones.substring(0, _phones.length - 1);
+      String _phones = recipients.join(",");
       mapData["recipients"] = _phones;
       final String result = await _channel.invokeMethod('sendSMS', mapData);
       return result;
