83f9d2fd25593ed628836b1ecbabb4a266f48f5f
Rody Davis <rody.davis.jr@gmail.com>
2019-05-21 12:47:10 -0400

    Update main_page.dart

--- packages/flutter_bootstrap/example/lib/pages/main_page.dart
+++ packages/flutter_bootstrap/example/lib/pages/main_page.dart
@@ -7,9 +7,58 @@ class MainPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final _media = MediaQuery.of(context).size;
-    print(_media);
     return LayoutBuilder(
       builder: (BuildContext context, BoxConstraints constraints) {
+        return Material(
+            child: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            SideBarMenu(menuItems: menuItems),
+            Expanded(
+              child: Scaffold(
+                appBar: AppBar(
+                  elevation: 4,
+                  centerTitle: true,
+                  title: Text(
+                    "Flutter Bootstrap Example",
+                  ),
+                  backgroundColor: drawerBgColor,
+                ),
+                body: Container(
+                  padding: EdgeInsets.all(20.0),
+                  child: bpRow(
+                    gutter: 20,
+                    constraints: constraints,
+                    runSpacing: 20,
+                    children: [
+                      bpCol(
+                        w360: 6,
+                        w720: 6,
+                        w1200: 7,
+                        w2000: 8,
+                        w2500: 9,
+                        constraints: constraints,
+                        child: new _MainSection(),
+                      ),
+                      bpCol(
+                        w360: 6,
+                        w720: 6,
+                        w1200: 5,
+                        w2000: 4,
+                        w2500: 3,
+                        constraints: constraints,
+                        child: Container(
+                          child: QuickContact(media: _media),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ));
         if (constraints.maxWidth <= 800) {
           return Material(
             child: Center(
@@ -191,6 +240,135 @@ class MainPage extends StatelessWidget {
           );
         }
       },
+    );
+  }
+}
+
+class _MainSection extends StatelessWidget {
+  const _MainSection({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final _media = MediaQuery.of(context).size;
+    return LayoutBuilder(
+        builder: (BuildContext context, BoxConstraints constraints) =>
+            Container(
+              child: bpRow(
+                gutter: 20,
+                constraints: constraints,
+                runSpacing: 20,
+                children: [
+                  _buildCardTile(
+                    constraints,
+                    CardTile(
+                      iconBgColor: Colors.orange,
+                      cardTitle: "Booking",
+                      icon: Icons.flight_takeoff,
+                      subText: "Todays",
+                      mainText: "230",
+                    ),
+                  ),
+                  _buildCardTile(
+                    constraints,
+                    CardTile(
+                      iconBgColor: Colors.pinkAccent,
+                      cardTitle: "Website Visits",
+                      icon: Icons.show_chart,
+                      subText: "Tracked from Google Analytics",
+                      mainText: "3.560",
+                    ),
+                  ),
+                  _buildCardTile(
+                    constraints,
+                    CardTile(
+                      iconBgColor: Colors.green,
+                      cardTitle: "Revenue",
+                      icon: Icons.home,
+                      subText: "Last 24 Hours",
+                      mainText: "2500",
+                    ),
+                  ),
+                  _buildCardTile(
+                    constraints,
+                    CardTile(
+                      iconBgColor: Colors.lightBlueAccent,
+                      cardTitle: "Followors",
+                      icon: Icons.unfold_less,
+                      subText: "Last 24 Hours",
+                      mainText: "112",
+                    ),
+                  ),
+                  _buildChartTile(
+                    constraints,
+                    ChartCardTile(
+                      cardColor: Color(0xFF7560ED),
+                      cardTitle: "Bandwidth usage",
+                      subText: "March 2017",
+                      icon: Icons.pie_chart,
+                      typeText: "50 GB",
+                    ),
+                  ),
+                  _buildChartTile(
+                    constraints,
+                    ChartCardTile(
+                      cardColor: Color(0xFF25C6DA),
+                      cardTitle: "Download count",
+                      subText: "March 2017",
+                      icon: Icons.cloud_upload,
+                      typeText: "35487",
+                    ),
+                  ),
+                  _buildProjectsTile(
+                      constraints,
+                      ProjectWidget(
+                        media: _media,
+                        projectItems: projectItems,
+                      )),
+                ],
+              ),
+            ));
+  }
+
+  bpCol _buildCardTile(BoxConstraints constraints, CardTile child) {
+    return bpCol(
+      w720: 6,
+      w1200: 4,
+      w2000: 3,
+      w2500: 2,
+      constraints: constraints,
+      child: Container(
+        height: 300,
+        child: child,
+      ),
+    );
+  }
+
+  bpCol _buildChartTile(BoxConstraints constraints, Widget child) {
+    print(constraints.maxWidth);
+    return bpCol(
+      w720: 12,
+      w1200: 4,
+      w2000: 4,
+      w2500: 4,
+      constraints: constraints,
+      child: Container(
+        child: child,
+      ),
+    );
+  }
+
+  bpCol _buildProjectsTile(BoxConstraints constraints, ProjectWidget child) {
+    return bpCol(
+      w720: 12,
+      w1200: 12,
+      w2000: 6,
+      w2500: 6,
+      constraints: constraints,
+      child: Container(
+        child: child,
+      ),
     );
   }
 }
