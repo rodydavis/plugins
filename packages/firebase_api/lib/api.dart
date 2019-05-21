ac6d82f340c6e377ed358256b3521f7c6d60b9eb
Rody Davis <rody.davis.jr@gmail.com>
2019-05-20 22:13:44 -0400

    moving to folder

--- packages/firebase_api/lib/api.dart
+++ packages/firebase_api/lib/api.dart
@@ -12,8 +12,8 @@ import 'src/impl/unsupported.dart'
 part 'src/client.dart';
 part 'src/endpoints.dart';
 part 'src/token.dart';
-part 'src/types/collection_reference.dart';
-part 'src/types/document_reference.dart';
-part 'src/types/document_snapshot.dart';
+part 'src/types/firestore/collection_reference.dart';
+part 'src/types/firestore/document_reference.dart';
+part 'src/types/firestore/document_snapshot.dart';
+part 'src/types/firestore/reference.dart';
 part 'src/types/object.dart';
-part 'src/types/reference.dart';
