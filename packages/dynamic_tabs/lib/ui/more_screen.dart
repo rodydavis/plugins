484f975f4e98fc70a8f7c5731a93fa05245ef8ce
Rody Davis <rody.davis.jr@gmail.com>
2019-05-28 14:11:23 -0400

    Update more_screen.dart

--- packages/dynamic_tabs/lib/ui/more_screen.dart
+++ packages/dynamic_tabs/lib/ui/more_screen.dart
@@ -25,7 +25,7 @@ class MoreTab extends StatelessWidget {
     @required this.accentColor,
     @required this.navigator,
     @required this.tabState,
-    this.breakpoint = 400,
+    this.breakpoint = 720,
   }) : masterDetail = true;
 
   final bool adaptive;
@@ -39,83 +39,87 @@ class MoreTab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListenableProvider<TabState>(
-      builder: (_) => tabState,
-      child: Consumer<TabState>(
-        builder: (context, model, child) {
-          if (adaptive && Platform.isIOS) {
-            if (masterDetail) {
-              return Row(
-                children: <Widget>[
-                  Container(
-                    width: 400,
-                    child: CupertinoPage(
-                      primaryColor: primaryColor,
-                      accentColor: accentColor,
-                      masterDetail: masterDetail,
-                      tabs: model.extraTabs,
-                      navigator: navigator,
-                      onEdit: () => _goToEditScreen(context, model),
-                      onTap: model.changeTab,
+        builder: (_) => tabState,
+        child: LayoutBuilder(
+          builder: (context, constraints) {
+            return Consumer<TabState>(
+              builder: (context, model, child) {
+                if (adaptive && Platform.isIOS) {
+                  if (MediaQuery.of(context).orientation ==
+                          Orientation.landscape &&
+                      masterDetail &&
+                      constraints.maxWidth >= breakpoint) {
+                    return Row(
+                      children: <Widget>[
+                        Container(
+                          width: 400,
+                          child: CupertinoPage(
+                            primaryColor: primaryColor,
+                            accentColor: accentColor,
+                            masterDetail: masterDetail,
+                            navigator: navigator,
+                            onEdit: () => _goToEditScreen(context, model),
+                            onTap: model.changeTab,
+                          ),
+                        ),
+                        Expanded(child: model.child),
+                      ],
+                    );
+                  }
+                  return CupertinoPage(
+                    primaryColor: primaryColor,
+                    accentColor: accentColor,
+                    masterDetail: masterDetail,
+                    navigator: navigator,
+                    onEdit: () => _goToEditScreen(context, model),
+                    onTap: (index) async {
+                      model.changeTab(index);
+                      await navigator.push(
+                        CupertinoPageRoute(
+                          builder: (context) => model.child,
+                        ),
+                      );
+                    },
+                  );
+                }
+                return Scaffold(
+                    appBar: AppBar(
+                      title: Text("More"),
+                      actions: <Widget>[
+                        IconButton(
+                          icon: Icon(Icons.edit),
+                          onPressed: () => _goToEditScreen(context, model),
+                        ),
+                      ],
                     ),
-                  ),
-                  Expanded(child: model.child),
-                ],
-              );
-            }
-            return CupertinoPage(
-              primaryColor: primaryColor,
-              accentColor: accentColor,
-              tabs: model.extraTabs,
-              masterDetail: masterDetail,
-              navigator: navigator,
-              onEdit: () => _goToEditScreen(context, model),
-              onTap: (index) async {
-                model.changeTab(index);
-                await navigator.push(
-                  CupertinoPageRoute(
-                    builder: (context) => model.child,
-                  ),
-                );
+                    body: SafeArea(
+                      child: ListView.builder(
+                        padding: EdgeInsets.all(0.0),
+                        itemCount: model.extraTabs.length,
+                        itemBuilder: (BuildContext context, int index) {
+                          final i = model.extraTabs[index];
+                          return ListTile(
+                            leading: i.tab.icon,
+                            title: i.tab.title,
+                            trailing: !masterDetail && adaptive
+                                ? Icon(Icons.keyboard_arrow_right)
+                                : null,
+                            onTap: () async {
+                              model.changeTab(index);
+                              await navigator.push(
+                                MaterialPageRoute(
+                                  builder: (context) => i.child,
+                                ),
+                              );
+                            },
+                          );
+                        },
+                      ),
+                    ));
               },
             );
-          }
-          return Scaffold(
-              appBar: AppBar(
-                title: Text("More"),
-                actions: <Widget>[
-                  IconButton(
-                    icon: Icon(Icons.edit),
-                    onPressed: () => _goToEditScreen(context, model),
-                  ),
-                ],
-              ),
-              body: SafeArea(
-                child: ListView.builder(
-                  padding: EdgeInsets.all(0.0),
-                  itemCount: model.extraTabs.length,
-                  itemBuilder: (BuildContext context, int index) {
-                    final i = model.extraTabs[index];
-                    return ListTile(
-                      leading: i.tab.icon,
-                      title: i.tab.title,
-                      trailing: !masterDetail && adaptive
-                          ? Icon(Icons.keyboard_arrow_right)
-                          : null,
-                      onTap: () async {
-                        model.changeTab(index);
-                        await navigator.push(
-                          MaterialPageRoute(
-                            builder: (context) => i.child,
-                          ),
-                        );
-                      },
-                    );
-                  },
-                ),
-              ));
-        },
-      ),
-    );
+          },
+        ));
   }
 
   void _goToEditScreen(BuildContext context, TabState state) {
@@ -145,7 +149,6 @@ class CupertinoPage extends StatelessWidget {
     @required this.navigator,
     @required this.onTap,
     @required this.onEdit,
-    @required this.tabs,
     @required this.masterDetail,
   }) : super(key: key);
 
@@ -154,61 +157,66 @@ class CupertinoPage extends StatelessWidget {
   final NavigatorState navigator;
   final ValueChanged<int> onTap;
   final VoidCallback onEdit;
-  final List<DynamicTab> tabs;
   final bool masterDetail;
 
   @override
   Widget build(BuildContext context) {
-    return CupertinoPageScaffold(
-      child: CustomScrollView(
-        slivers: <Widget>[
-          CupertinoSliverNavigationBar(
-            backgroundColor: primaryColor,
-            actionsForegroundColor: accentColor,
-            transitionBetweenRoutes: !masterDetail,
-            heroTag: Key('more-tab'),
-            largeTitle: Text("More"),
-            trailing: CupertinoButton(
-              child: Text("Edit"),
-              padding: EdgeInsets.all(0.0),
-              onPressed: onEdit,
+    return Consumer<TabState>(
+      builder: (context, model, child) => CupertinoPageScaffold(
+            child: CustomScrollView(
+              slivers: <Widget>[
+                CupertinoSliverNavigationBar(
+                  backgroundColor: primaryColor,
+                  actionsForegroundColor: accentColor,
+                  transitionBetweenRoutes: !masterDetail,
+                  heroTag: Key('more-tab'),
+                  largeTitle: Text("More"),
+                  trailing: CupertinoButton(
+                    child: Text("Edit"),
+                    padding: EdgeInsets.all(0.0),
+                    onPressed: onEdit,
+                  ),
+                ),
+                SliverPadding(
+                  padding: MediaQuery.of(context)
+                      .removePadding(
+                        removeTop: true,
+                        removeLeft: true,
+                        removeRight: true,
+                      )
+                      .padding,
+                  sliver: SliverList(
+                    delegate: SliverChildBuilderDelegate(
+                      (BuildContext context, int index) {
+                        final i = model.extraTabs[index];
+                        final Icon _icon = i.tab.icon;
+                        final Text _text = i.tab.title;
+                        return DefaultTextStyle(
+                            style:
+                                CupertinoTheme.of(context).textTheme.textStyle,
+                            child: CupertinoListTile(
+                              leading: _icon.icon,
+                              title: _text,
+                              selected: masterDetail
+                                  ? index == model.currentIndex
+                                  : false,
+                              lastItem: false,
+                              ios: CupertinoListTileData(
+                                style: CupertinoCellStyle.subtitle,
+                                accessory: masterDetail
+                                    ? CupertinoAccessory.none
+                                    : CupertinoAccessory.disclosureIndicator,
+                              ),
+                              onTap: () => onTap(index),
+                            ));
+                      },
+                      childCount: model.extraTabs.length,
+                    ),
+                  ),
+                ),
+              ],
             ),
           ),
-          SliverPadding(
-            padding: MediaQuery.of(context)
-                .removePadding(
-                  removeTop: true,
-                  removeLeft: true,
-                  removeRight: true,
-                )
-                .padding,
-            sliver: SliverList(
-              delegate: SliverChildBuilderDelegate(
-                (BuildContext context, int index) {
-                  final i = tabs[index];
-                  final Icon _icon = i.tab.icon;
-                  final Text _text = i.tab.title;
-                  return DefaultTextStyle(
-                      style: CupertinoTheme.of(context).textTheme.textStyle,
-                      child: CupertinoListTile(
-                        leading: _icon.icon,
-                        title: _text,
-                        lastItem: false,
-                        ios: CupertinoListTileData(
-                          style: CupertinoCellStyle.subtitle,
-                          accessory: masterDetail
-                              ? CupertinoAccessory.none
-                              : CupertinoAccessory.disclosureIndicator,
-                        ),
-                        onTap: () => onTap(index),
-                      ));
-                },
-                childCount: tabs.length,
-              ),
-            ),
-          ),
-        ],
-      ),
     );
   }
 }
