6e7746d808cd049b644022c51f03d155b1f34fc0
Rody Davis <rody.davis.jr@gmail.com>
2019-06-25 15:30:02 -0400

    updating

--- packages/floating_search_bar/example/lib/main.dart
+++ packages/floating_search_bar/example/lib/main.dart
@@ -40,6 +40,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData.dark(),
       home: FloatingSearchBar.builder(
         itemCount: 100,
         itemBuilder: (BuildContext context, int index) {
